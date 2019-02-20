e49d6fe61f0e1d3ebba8f0b6784690339183f0e6
Aleksander Dietrichson <dietrichson@gmail.com>
2019-02-20 17:11:45 -0300

    Refactored plot tests with vdiffr package. These are now integrated with testthat tests.

---
+++ tests/testthat/test_plot_functions.R
@@ -0,0 +1,30 @@
+context('Testing Plot Functions')
+library(egvisualizations)
+library(vdiffr)
+myData <- readRDS('./Argentina-2019-01-25.RDS')
+
+expect_doppelganger(title = 'egbarplotcount',
+                    egbarplotcount(myData$news, "media_name", "..count..", "media_name","a","b", "test"))
+# This plot is not testable with vdiffr
+expect_doppelganger(title = 'egwordcloud',
+                    {
+                      set.seed(40)
+                      a <- myData$news_wordcloud %>% group_by(word) %>% count()
+                      egwordcloud(a$word %>% head(20), a$n %>% head(20))
+                    })
+
+expect_doppelganger(title = 'egdataseries',
+                    egdataseries(myData$tweets, "created_at", "screen_name","Date","Count","Candidate")
+)
+
+expect_doppelganger(title = 'egdataseries2',
+                    egdataseries(myData$youtube_videos, "publication_date", NA,"Date","Count","Candidate")
+)
+
+expect_doppelganger(title = 'egheatmap',
+                    {a <- egdataanalysis::network_structure_relation_mentions(myData$entities, 
+                                                                              myData$news_entities_mentions)
+                    test <- a[[2]] %>% left_join(a[[1]] %>% select(id, title) %>% rename(totitle = title), by = c("to"="id")) %>% left_join(a[[1]] %>% select(id, title) %>% rename(fromtitle = title), by = c("from"="id"))
+                    
+                    egvisualizations::egheatmap(test, "totitle","fromtitle","length")}
+)

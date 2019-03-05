35fac2b0d192add9e1c24b189c614524f2a99c4b
Aleksander Dietrichson <dietrichson@gmail.com>
2019-03-05 10:36:15 -0300

    r

--- tests/testthat/test_plot_functions_for_polls.R
+++ tests/testthat/test_plot_functions_for_polls.R
@@ -1,37 +1,12 @@
-context('Testing Plot Functions for Polls - Timelines')
 library(egvisualizations)
 library(vdiffr)
 library(dplyr)
 myData <- readRDS('./poll_test_data.RDS')
 
 
-expect_error(eg_poll_timeline())# Empty call
-
-expect_error(eg_poll_timeline(myData %>% select(fullname))) #Missing columns
-
-#Test the plot functions
-expect_doppelganger('Timeline One Candidate Approval/Disapproval',
-                      myData %>% 
-                      dplyr::filter(shortname=='aBarnecheaG') %>% 
-                      eg_poll_timeline()
-                    )
-
-expect_doppelganger('Timeline One Candidate One Variable',
-                      myData %>% 
-                      dplyr::filter(shortname=='aBarnecheaG',variable=='Approval') %>% 
-                      eg_poll_timeline()
-                    )
-
-expect_doppelganger('Timeline All Candidates Approval/Disapproval',
-                    myData %>% eg_poll_timeline()
-)
-
-expect_doppelganger('Timeline All Candidates Approval Only',
-                    myData %>% dplyr::filter(variable=='Approval') %>% eg_poll_timeline()
-)
-
 # Test Boxplots
 context('Testing Plot Functions for Polls - Boxplots')
+
 expect_error(eg_poll_boxplot(),
              label = 'eg_poll_boxplot called empty.')# Empty call
 
@@ -58,3 +33,4 @@ expect_doppelganger('Boxplot One Candidate One Variable',
                       dplyr::filter(shortname=='aBarnecheaG',variable=='Approval') %>% 
                       eg_poll_boxplot()
                     )
+
